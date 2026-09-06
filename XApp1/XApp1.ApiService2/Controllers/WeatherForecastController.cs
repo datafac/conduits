477b9c6ff0ed61bcbfcbc52bb1a5d1c364cafd80
Psiman62 <psiman-hju@bytz.me>
2026-09-06 15:05:38 +1000

    route webfrontend to apiService2 to grpcService2

--- XApp1/XApp1.ApiService2/Controllers/WeatherForecastController.cs
+++ XApp1/XApp1.ApiService2/Controllers/WeatherForecastController.cs
@@ -7,12 +7,25 @@ namespace XApp1.ApiService2.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController : ControllerBase, IAsyncDisposable
 {
     private static string GetServiceAddress(string variableName)
     {
         return Environment.GetEnvironmentVariable(variableName)
             ?? throw new InvalidOperationException($"Environment variable '{variableName}' is not set or invalid.");
+    }
+
+    private readonly string _weatherSvcAddress = GetServiceAddress("GRPCSERVICE2_HTTPS");
+    private readonly WeatherClient _weatherSvc;
+
+    public WeatherForecastController()
+    {
+        _weatherSvc = new WeatherClient(new ProtocolClient(new GrpcConduitClient(_weatherSvcAddress)));
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _weatherSvc.DisposeAsync();
     }
 
     private static readonly string[] Summaries =
@@ -21,17 +34,18 @@ public class WeatherForecastController : ControllerBase
     ];
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> GetAll()
+    public async IAsyncEnumerable<WeatherForecast> GetAll()
     {
-        //await using var weatherSvc = new WeatherClient(new ProtocolClient(new GrpcConduitClient(GetServiceAddress("GRPCSERVICE2_HTTPS"))));
-        //var weather = await weatherSvc.GetWeather(12345);
+        var weather = await _weatherSvc.GetWeather(12345);
 
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        await foreach (var wd in _weatherSvc.GetForecast(0, 7))
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            yield return new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(new DateTime(wd.DateTimeUtc, DateTimeKind.Utc)),
+                TemperatureC = wd.TemperatureC,
+                Summary = wd.Summary
+            };
+        }
     }
 }
