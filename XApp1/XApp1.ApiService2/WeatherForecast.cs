e4bab19faddeb0387dd47ad0dd0552d9721b50aa
Psiman62 <psiman-hju@bytz.me>
2026-09-07 21:26:15 +1000

    remove unused apiService 1 & 2

--- XApp1/XApp1.ApiService2/WeatherForecast.cs
+++
@@ -1,6 +0,0 @@
-namespace XApp1.ApiService2;
-
-public class JsonMessage
-{
-    public byte[]? Payload { get; set; }
-}
--- XApp1/XApp1.AppHost/AppHost.cs
+++ XApp1/XApp1.AppHost/AppHost.cs
@@ -4,22 +4,6 @@ var grpcService1 = builder.AddProject<Projects.XApp1_GrpcService1>("grpcservice1
 
 var grpcService2 = builder.AddProject<Projects.XApp1_GrpcService2>("grpcservice2");
 
-var apiService = builder.AddProject<Projects.XApp1_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health")
-    .WithReference(grpcService1)
-    .WithReference(grpcService2)
-    .WaitFor(grpcService1)
-    .WaitFor(grpcService2)
-    ;
-
-var apiService2 = builder.AddProject<Projects.XApp1_ApiService2>("apiservice2")
-    .WithHttpHealthCheck("/health")
-    .WithReference(grpcService1)
-    .WithReference(grpcService2)
-    .WaitFor(grpcService1)
-    .WaitFor(grpcService2)
-    ;
-
 var apiService3 = builder.AddProject<Projects.XApp1_ApiService3>("apiservice3")
     .WithHttpHealthCheck("/health")
     .WithReference(grpcService1)
@@ -31,10 +15,6 @@ var apiService3 = builder.AddProject<Projects.XApp1_ApiService3>("apiservice3")
 builder.AddProject<Projects.XApp1_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
-    .WaitFor(apiService)
-    .WithReference(apiService2)
-    .WaitFor(apiService2)
     .WithReference(apiService3)
     .WaitFor(apiService3)
     ;
